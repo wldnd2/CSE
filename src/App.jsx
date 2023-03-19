df9d734fa9df794b5a62f9e5aee3a5b04078e7bc
wldnd2 <jiwoong000628@gmail.com>
2023-03-19 18:27:15 +0900

    design: Tailwind Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CheckDues from "./components/CheckDues";
 import ShowResult from "./components/ShowResult";
+import "./index.css";
 
 const App = () => {
   return (
